059320721bdf9bf24a34175ee51dba7bc60d2d8a
parasuram1990 <parasuram499@gmail.com>
2025-04-02 16:22:11 +0530

    1st js class

---
+++ main.js
@@ -0,0 +1,12 @@
+let name = "your name";
+let num = 1;
+let array = [1,2,3];
+let object ={
+    name: "your name",
+    age: 1,
+};
+console.log("log", name);
+name = "new value";
+console.log(name);
+// let body = document.getElementsByTagName("body");
+// body[0].innerHTML="your name";
