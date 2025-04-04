e526a13f86d1d5bb4f1fe56451380b6aa9e6f358
parasuram1990 <parasuram499@gmail.com>
2025-04-04 16:22:11 +0530

    js

--- input.js
+++ input.js
@@ -3,4 +3,22 @@ function submit() {
     console.log(input[0].value);
     let name = document.getElementById("name");
     console.log(name.value);
+    let checkbox = document.getElementById("checkbox");
+    console.log(checkbox.value);
+    let date = document.getElementById("date");
+    console.log(date.value);
+    let datetime = document.getElementById("datetime");
+    console.log(datetime.value);
+    let email = document.getElementById("email");
+    console.log(email.value);
+    let password = document.getElementById("password");
+    console.log(password.vbalue);
+    let radio = document.getElementById("radio")
+    console.log(radio.value);
+    let textarea = document.getElementById("textarea");
+    console.log(textarea.value);
+    
 }
+        
+
+
